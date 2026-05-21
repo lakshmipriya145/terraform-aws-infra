cb6f672813c690a1fd714039a5c0a4f86573c589
Priya <lakshmipriya01555@gmail.com>
2026-05-21 12:10:30 +0530

    Initial Terraform AWS project

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "devops_server" {
+
+  ami           = "ami-0f58b397bc5c1f2e8"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "PriyaTerraformServer"
+  }
+
+}
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "ec2_public_ip" {
+
+  value = aws_instance.devops_server.public_ip
+
+}
